56a5fd529a6f32aaedacc7e2d46448a81e201891
Zandanbat Ts <zandanbat.ts@gazar.local>
2025-12-30 12:26:03 +0800

    Add kanban page and update urls/views

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+def home(request):
+    return redirect('my_tasks')
 
 urlpatterns = [
+    path('', home),              # 👈 ЭНЭ ЧУХАЛ
     path('admin/', admin.site.urls),
     path('', include('tasks.urls')),
 ]
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
-from .views import (
-    my_tasks,
-    update_task,
-    manager_dashboard,
-    export_tasks_excel
-)
+from . import views
 
 urlpatterns = [
-    path('my-tasks/', my_tasks, name='my_tasks'),
-    path('update-task/<int:task_id>/', update_task, name='update_task'),
-    path('dashboard/', manager_dashboard, name='dashboard'),
-    path('export-excel/', export_tasks_excel, name='export_excel'),
+    path('my-tasks/', views.my_tasks, name='my_tasks'),
+    path('kanban/', views.kanban_board, name='kanban'),
+    path('move-task/<int:task_id>/<str:status>/', views.move_task, name='move_task'),
 ]
